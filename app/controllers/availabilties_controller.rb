b591ee50ad417e25b59e3448dc37fd81c8f03c35
kevin rossi <kevinrossi@free.fr>
2024-09-28 15:38:40 +0200

    navbar with actions and controllers

--- app/controllers/availabilties_controller.rb
+++ app/controllers/availabilties_controller.rb
@@ -1,11 +1,24 @@
 class AvailabiltiesController < ApplicationController
   def index
-
+    @availabilities = Availability.all
   end
 
-  def new
+  def create
+    @availability = Availability.new(availability_params)
+    @availability.user = current_user
+    if @availability.save
+      redirect_to users_path(current_user)
+    else
+      render 'new'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def availability_params
+    params.require(:availability).permit(:day, :month, :year, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resource :users, only: [:show] do
-    resources :availabilities, only: [:index, :new, :destroy]
+  resources :users, only: [:show] do
+    resources :availabilities, only: [:index, :create, :destroy]
   end
 
   resources :cares, only: [:index, :show, :new, :create, :edit, :update]
