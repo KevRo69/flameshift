71ccc0e3a3cf040dc037185a538f279f55996af9
kevin rossi <kevinrossi@free.fr>
2024-09-29 21:57:58 +0200

    added flatpickr to show availabilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
     # @cares_month = @user.cares.where(month: Date.today.month, year: Date.today.year)
     start_of_next = Date.today.day < 16 ? Date.today.beginning_of_month + 1.months : Date.today.beginning_of_month + 2.months
     end_of_next = Date.today.day < 16 ? Date.today.end_of_month + 1.months : Date.today.end_of_month + 2.months
-    @availabilities_next = @user.availabilties.where(day: (start_of_next)..(start_of_next)).uniq{ |t| t.day}.sort_by(&:day)
+    @availabilities_next = @user.availabilties.where(day: (start_of_next)..(end_of_next)).uniq{ |t| t.day}.sort_by(&:day)
+    @availabilities_next_days = @availabilities_next.map { |date| date.day }
     # @cares_next = @user.cares.where(month: Date.today.month + 1, year: Date.today.year)
   end
 end
