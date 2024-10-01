cb4e87c344648acd5892101cdf09cbca0714ed4a
kevin rossi <kevinrossi@free.fr>
2024-10-01 11:35:45 +0200

    added edit availabilities (miss destroy)

--- app/controllers/availabilties_controller.rb
+++ app/controllers/availabilties_controller.rb
@@ -9,15 +9,29 @@ class AvailabiltiesController < ApplicationController
   end
 
   def create
+    start_of_next = Date.today.day < 16 ? Date.today.beginning_of_month + 1.months : Date.today.beginning_of_month + 2.months
+    end_of_next = Date.today.day < 16 ? Date.today.end_of_month + 1.months : Date.today.end_of_month + 2.months
+    availabilities_next = current_user.availabilties.where(day: (start_of_next)..(end_of_next))
     availabilities = availability_params[:day].split(", ")
+    puts availabilities
     availabilities.each do |day|
-      @availability = Availabilty.new(day: day, user: current_user)
-      @availability.save
+      if availabilities_next.select { |t| t.day.strftime('%Y-%m-%d') == day }.empty?
+        availability = Availabilty.new(day: day, user: current_user)
+        availability.save
+      end
     end
     redirect_to user_path(current_user)
   end
 
-  def destroy
+  def edit
+    @user = current_user
+    @availability = Availabilty.new
+
+    start_of_next = Date.today.day < 16 ? Date.today.beginning_of_month + 1.months : Date.today.beginning_of_month + 2.months
+    end_of_next = Date.today.day < 16 ? Date.today.end_of_month + 1.months : Date.today.end_of_month + 2.months
+
+    @availabilities_next = @user.availabilties.where(day: (start_of_next)..(end_of_next)).uniq { |t| t.day }.sort_by(&:day)
+    @availabilities_next_days = @availabilities_next.map { |date| date.day }
   end
 
   private
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    start_of_month = Date.today.day < 16 ? Date.today.beginning_of_month + 1.months : Date.today.beginning_of_month + 2.months
+    end_of_month = Date.today.day < 16 ? Date.today.end_of_month + 1.months : Date.today.end_of_month + 2.months
+    days = (start_of_month..end_of_month).to_a
+    @cares = Care.where(day: days)
   end
 end
