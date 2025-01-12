4f2b4a78cbd4bad8e82e5df4c92af23402f14d82
kevin rossi <kevinrossi@free.fr>
2025-01-12 19:15:23 +0100

    fixed messages problems

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_many :availabilties, dependent: :destroy
   has_many :user_maneuvers, dependent: :destroy
   validates :first_name, :last_name, presence: true
+  validates :password, presence: true, confirmation: true, length: { within: 6..128 }, if: -> { password.present? }
   accepts_nested_attributes_for :user_maneuvers, allow_destroy: true
 
   def active_for_authentication?
