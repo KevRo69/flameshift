3205a7644c2dfb339aa67246e29429d776b346de
kevin rossi <kevinrossi@free.fr>
2025-07-27 20:14:13 +0200

    added condition to js controllers

--- app/javascript/controllers/datepicker_show_care_actual_controller.js
+++ app/javascript/controllers/datepicker_show_care_actual_controller.js
@@ -39,8 +39,9 @@ export default class extends Controller {
         defaultDate: this.caresValue,
         "disable": [
         function(date) {
-            // return true to disable
+          if (!(month === 6 || month === 7)) {
             return (date.getDay() === 1 || date.getDay() === 2 || date.getDay() === 3 || date.getDay() === 4);
+          };
             }
         ],
         "locale": {
