b0e1057087d45e0ac07d3e86fa42eb2f88d5b659
kevin rossi <kevinrossi@free.fr>
2025-09-14 22:02:00 +0200

    added monthly availabilities and fixed for yearly

--- app/services/availabilties/create_yearly_availabilities.rb
+++ app/services/availabilties/create_yearly_availabilities.rb
@@ -7,14 +7,14 @@ module Availabilties
     def execute
       start_date = Date.today.beginning_of_month >> 1
       end_date = (start_date >> 12) - 1.days
-      create_availabilties(@user, start_date, end_date)
+      create_availabilties(start_date, end_date)
     end
 
     private
 
-    def create_availabilties(user, start_date, end_date)
+    def create_availabilties(start_date, end_date)
       (start_date..end_date).each do |date|
-        user.availabilties.find_or_create_by(day: date)
+        @user.availabilties.find_or_create_by(day: date)
       end
     end
   end
